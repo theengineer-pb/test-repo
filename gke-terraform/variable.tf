c7bfe1f197718df503d38ba944ea5c95503fd494
Pratham34 <99575732+Pratham34@users.noreply.github.com>
2026-05-31 09:43:44 +0530

    Update default GCP project ID in variable.tf

--- gke-terraform/variable.tf
+++ gke-terraform/variable.tf
@@ -1,7 +1,7 @@
 variable "project" {
   description = "this is gcp project-id"
   type        = string
-  default     = "rock-loop-449918-b0"
+  default     = "devsecops-proj-497502"
 }
 
 variable "region" {
